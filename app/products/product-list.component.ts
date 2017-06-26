37ebc4e226446c10520b74d5082964d9bd7516ab
Mainak Dhar <mainak90@gmail.com>
2017-06-27 04:07:20 +0530

    1st commit(incomplete)

---
+++ app/products/product-list.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { IProduct } from './product';
+import { ProductService } from './product.service';
+@Component({
+    'selector' : 'pm-products',
+    'templateUrl' : 'app/products/product-list.component.html',
+    'styleUrls' : ['app/products/product-list.component.css']
+})
+
+export class ProductListComponent implements OnInit { 
+    pageTitle : string = 'Product List!';
+    imageWidth : number = 50;
+    imageMargin : number = 2;
+    listFilter: string;
+    showImage : boolean = false;
+    products : IProduct[];
+
+    constructor(private _productService : ProductService) {
+
+    }
+
+    toggleImage() : void { 
+        this.showImage = !this.showImage; 
+    }
+    ngOnInit():void {
+        this.products = this._productService.getProducts();
+    }
+
+    onRatingClicked(message : string) : void {
+        this.pageTitle= 'Product List : ' + message;
+    }
+
+}
---
+++ app/products/product.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { IProduct } from './product';
+import { Http, Response } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/app/operator/map';
+
+@Injectable()
+export class ProductService{
+    private _productUrl : 'api/products/products.json';
+    constructor(private _http : Http){
+
+    }
+    getProducts(): Observable<IProduct[]>{
+        return this._http.get(this._productUrl);
+        map((response : Response) => <IProduct[]> response.json()); 
+    }
+} 
